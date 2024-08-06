1c2684ba9b9e2355cdd55991a7e1037b5551bbdb
Maksym Protsak <maksym.protsak@gmail.com>
2024-08-06 16:59:17 +0300

    Solution 06-08-2024 16-59

--- taxi/models.py
+++ taxi/models.py
@@ -1,3 +1,14 @@
+from django.contrib.auth.models import AbstractUser
 from django.db import models
 
-# Create your models here.
+
+class Manufacturer(models.Model):
+    name = models.CharField(max_length=100, unique=True)
+    country = models.CharField(max_length=100)
+
+
+class Driver(AbstractUser):
+    license_number = models.CharField(max_length=255, unique=True)
+    username = models.CharField(max_length=100)
+    email = models.EmailField()
+    password = models.CharField(max_length=100)
